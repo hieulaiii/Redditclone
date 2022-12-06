ae202a7f1d5d659178336295311dbb84c3a72ac8
hieulaiii <113016582+hieulaiii@users.noreply.github.com>
2022-12-06 17:17:47 +0700

    Update side-bar.component.ts

--- src/app/shared/side-bar/side-bar.component.ts
+++ src/app/shared/side-bar/side-bar.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-side-bar',
   templateUrl: './side-bar.component.html',
   styleUrls: ['./side-bar.component.css']
 })
-export class SideBarComponent {
+export class SideBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  goToCreatePost(){}
+  goToCreateSubreddit(){}
 
 }
